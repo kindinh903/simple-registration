40d310e17f728b8e7f8f2bdbc10453dcd4e8af32
kindinh903 <kindinh903@gmail.com>
2025-11-14 01:13:20 +0700

    modify fe api calling

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const registerUser = async (data) => {
-  const res = await axios.post(`${API_BASE_URL}/user/register`, data);
+  // const res = await axios.post(`${API_BASE_URL}/user/register`, data);
+  const res = await axios.post(`https://simple-registration-be.vercel.app/user/register`, data);
   return res.data;
 };
